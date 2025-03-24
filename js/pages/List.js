f32a69b6c039e8b98432b280ab6de314608a27d7
HYPERplanet7 <sa8400201@gmail.com>
2025-03-24 19:35:40 +0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,19 +100,19 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        level must have timings/gaps that are possible on 7000 fps or less
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Level must be physically possible
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Level must have more than 100 cps
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        levels must be showcased without noclip
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        You cant copy levels and buff them, that would disqualify your level from level submission.
                     </p>
                     <p>
                         Do not use secret routes or bug routes
@@ -121,7 +121,7 @@ export default {
                         Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Levels must be 1-60 seconds long
                     </p>
                 </div>
             </div>
